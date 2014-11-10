a246a95cf54aa1fc352cd8f2959af1291bfe0718
rixnco <rxboucher@gmail.com>
2014-11-10 14:07:32 +0100

    Back to high score

--- src/tron/Strategy.java
+++
@@ -1,8 +0,0 @@
-package tron;
-
-
-public interface Strategy {
-	public Move nextMove(final Move present, final Grid g, final int player, int maxDepth, Watchdog watchdog);
-	public Integer evaluate(final Grid g, final int player);
-	
-}
